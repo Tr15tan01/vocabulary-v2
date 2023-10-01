fd296f0d19fa2b7a8cb90238310f6e4af80e8c2f
tr15tan01 <t.varamashvili@gmail.com>
2023-10-01 23:47:10 +0400

    added context folder

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { createContext } from "react";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const ThemeContext = createContext("light");
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ThemeContext.Provider value={"theme"}>
+      <App />
+    </ThemeContext.Provider>
+  </React.StrictMode>
+);
